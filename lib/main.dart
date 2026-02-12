d436884723667f18d2b517d35ac9168fad4a7f6e
a.23308051280433 <a.23308051280433@cbtis128.edu.mx>
2026-02-12 20:22:44 +0000

    Act8LuciaNava

--- lib/main.dart
+++ lib/main.dart
@@ -1,122 +1,134 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(AppCafeteria());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class AppCafeteria extends StatelessWidget {
+  const AppCafeteria({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      title: "Cafeteria",
+      home: Postres(),
     );
   }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+}//Fin clase app cafeteria
+class Postres extends StatelessWidget {
+  const Postres({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    return Scaffold(appBar: AppBar(
+        title: const Text(
+          'Cafetería 23 casi 24 Nava - 6º J',
+          style: TextStyle(color: Color.fromARGB(255, 0, 0, 0), fontWeight: FontWeight.bold),
+        ),
+        centerTitle: true,
+        backgroundColor: const Color.fromARGB(255, 238, 205, 195), // Café oscuro
+        leading: IconButton(
+          icon: const Icon(Icons.coffee, color: Color.fromARGB(255, 0, 0, 0)),
+          onPressed: () {},
+        ),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.shopping_cart, color: Color.fromARGB(255, 0, 0, 0)),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: const Icon(Icons.account_circle, color: Color.fromARGB(255, 0, 0, 0)),
+            onPressed: () {},
+          ),
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      body: ListView.builder(
+  padding: const EdgeInsets.all(16),
+  itemCount: 4,
+  itemBuilder: (context, index) {
+    // Lista actualizada con postres y colores cálidos
+    final List<Map<String, dynamic>> items = [
+      {
+        't': 'Tarta de Red Velvet', 
+        's': 'Bizcocho suave con crema de queso', 
+        'i': Icons.cake_outlined, 
+        'c': const Color.fromARGB(255, 247, 248, 211)
+      },
+      {
+        't': 'Muffin de Arándanos', 
+        's': 'Recién horneado y esponjoso', 
+        'i': Icons.bakery_dining_outlined, 
+        'c': const Color.fromARGB(255, 218, 205, 255)
+      },
+      {
+        't': 'Cheesecake de Maracuyá', 
+        's': 'Base de galleta y toque tropical', 
+        'i': Icons.icecream_outlined, 
+        'c': const Color.fromARGB(255, 255, 236, 202)
+      },
+      {
+        't': 'Brownie con Nueces', 
+        's': 'Chocolate intenso 70% cacao', 
+        'i': Icons.cookie_outlined, 
+        'c': const Color.fromARGB(255, 224, 190, 177)
+      },
+    ];
+
+    return Container(
+      margin: const EdgeInsets.only(bottom: 12),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(20),
+        boxShadow: [
+          BoxShadow(
+            color: items[index]['c'].withOpacity(0.1), // Sombra del color del postre
+            blurRadius: 10,
+            offset: const Offset(0, 4),
+          ),
+        ],
+      ),
+      child: ListTile(
+        contentPadding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        // 1. Leading: Icono representativo del postre
+        leading: Container(
+          padding: const EdgeInsets.all(8),
+          decoration: BoxDecoration(
+            color: items[index]['c'].withOpacity(0.15),
+            shape: BoxShape.circle,
+          ),
+          child: Icon(items[index]['i'], color: items[index]['c']),
+        ),
+        // 2. Title: Nombre del postre
+        title: Text(
+          items[index]['t'],
+          style: const TextStyle(
+            fontWeight: FontWeight.bold, 
+            fontSize: 16,
+            color: Color(0xFF2D2D2D) // Un gris muy oscuro casi negro
+          ),
+        ),
+        // 3. Subtitle: Descripción corta
+        subtitle: Text(items[index]['s']),
+        // 4. Trailing: Botón de "Agregar"
+        trailing: CircleAvatar(
+          radius: 18,
+          backgroundColor: items[index]['c'],
+          child: IconButton(
+            icon: const Icon(Icons.add, size: 18, color: Colors.white),
+            onPressed: () {
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  backgroundColor: items[index]['c'],
+                  content: Text('¡${items[index]['t']} añadido al carrito!'),
+                  duration: const Duration(seconds: 1),
+                ),
+              );
+            },
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  },
+),
+);
   }
-}
+}//Fin clase postres
